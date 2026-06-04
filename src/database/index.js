5f782f59af5d8fef85ccd17d8350701d210d2ab9
Daniel Bork <daniel.bork@yahoo.com.br>
2026-06-04 09:52:48 -0300

    fix: adjust file imports

--- src/database/index.js
+++ src/database/index.js
@@ -3,18 +3,29 @@ import configDatabase from '../config/database.js';
 import User from '../app/models/User.js';
 import Product from '../app/models/Product.js';
 import Category from '../app/models/Category.js';
+import mongoose from 'mongoose';
 
 const models = [User, Product, Category];
 
 class Database {
     constructor() {
         this.init();
+        this.mongo();
     }
 
     init() {
         this.connection = new Sequelize(configDatabase);
         models.forEach((model) => model.init(this.connection));
         models.forEach((model) => model.associate && model.associate(this.connection.models));
+    }
+
+    async mongo() {
+        try {
+            await mongoose.connect(process.env.MONGO_URL);
+            console.log('MongoDB conectado!');
+        } catch (err) {
+            console.warn('MongoDB não conectado:', err.message);
+        }
     }
 }
 export default new Database();
--- src/server.js
+++ src/server.js
@@ -1,4 +1,2 @@
-import 'dotenv/config';
-import './database/index.js';
 import app from './app.js';
 app.listen(process.env.PORT || 3001, () => console.log('Server is running...'));
